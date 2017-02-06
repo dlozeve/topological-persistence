19498b58654759a0c6f305675b5a76f6331ceafd
Dimitri Lozeve <dimitri.lozeve@gmail.com>
2017-02-06 19:13:22 +0100

    Timings for boundary matrix creation and reduction

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,15 @@ import java.util.Vector;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Vector<Simplex> F = new ReadFiltration().readFiltration("filtration_A.txt");
+        Vector<Simplex> F = new ReadFiltration().readFiltration("filtration_D.txt");
         //System.out.println(F.toString());
+        long startTime = System.nanoTime();
         BoundaryMatrix m = new BoundaryMatrix(F);
+        long creationTime = System.nanoTime() - startTime;
         m.reduction();
-        m.barcodeToFile("test.txt");
+        long reductionTime = System.nanoTime() - creationTime - startTime;
+        System.out.println(m.barcode());
+        System.out.println("Boundary matrix creation time: " + creationTime / 1000000. + " ms");
+        System.out.println("Reduction time: " + reductionTime / 1000000. + " ms");
     }
 }
