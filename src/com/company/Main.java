1e5be4773fab0968cfb4988f10d60e6f8a293c55
Dimitri Lozeve <dimitri.lozeve@gmail.com>
2017-02-07 14:25:01 +0100

    Export all barcodes to files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,14 +6,16 @@ import java.util.Vector;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Vector<Simplex> F = new ReadFiltration().readFiltration("filtration_D.txt");
+        String filtrationName = "filtration_A";
+        Vector<Simplex> F = new ReadFiltration().readFiltration(filtrationName + ".txt");
         //System.out.println(F.toString());
         long startTime = System.nanoTime();
         BoundaryMatrix m = new BoundaryMatrix(F);
         long creationTime = System.nanoTime() - startTime;
         m.reduction();
         long reductionTime = System.nanoTime() - creationTime - startTime;
-        System.out.println(m.barcode());
+        //System.out.println(m.barcode());
+        m.barcodeToFile(filtrationName + "_barcode.txt");
         System.out.println("Boundary matrix creation time: " + creationTime / 1000000. + " ms");
         System.out.println("Reduction time: " + reductionTime / 1000000. + " ms");
     }
