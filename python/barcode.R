0a006ca92c7d7d77d495c14a2d74a62e720b91de
Dimitri Lozeve <dimitri.lozeve@gmail.com>
2017-02-07 16:19:46 +0100

    Modified barcode visualization

--- python/barcode.R
+++ python/barcode.R
@@ -5,7 +5,8 @@ barcode <- read_delim("../filtration_C_barcode.txt", delim=' ',
 barcode
 
 ggplot(barcode, aes(ymin=inf, ymax=sup, x=dim, y=dim)) +
-    geom_linerange(position=position_jitter(.2)) +
-    coord_flip()
+    geom_linerange(position=position_jitter(.3)) +
+    coord_flip() +
+    scale_x_reverse()
 
 
