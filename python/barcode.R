89d8fbf95d77c8ce89047d5e0a52230508f1df6a
Dimitri Lozeve <dimitri.lozeve@gmail.com>
2017-02-07 14:25:55 +0100

    Generate barcode graphs

---
+++ python/barcode.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+barcode <- read_delim("../filtration_C_barcode.txt", delim=' ',
+                      col_names=c('dim', 'inf', 'sup'))
+barcode
+
+ggplot(barcode, aes(ymin=inf, ymax=sup, x=dim, y=dim)) +
+    geom_linerange(position=position_jitter(.2)) +
+    coord_flip()
+
+
